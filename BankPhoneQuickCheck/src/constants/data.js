a29d6cbad99cecbd2be51222ecf12b5de3833668
Johnny Gu <johnny@wiredcraft.com>
2018-01-17 19:06:30 +0800

    update data

--- BankPhoneQuickCheck/src/constants/data.js
+++ BankPhoneQuickCheck/src/constants/data.js
@@ -6,28 +6,42 @@ module.exports = function () {
     {
       "id": "00",
       "name": "中国银行白金信用卡",
-      "description": "4006695569",
+      "description": "40066-95569 -> 1 -> 0",
       "logo": "../static/images/logo.jpeg",
       "phoneNumber": "4006695569"
     },
     {
       "id": "01",
+      "name": "中国银行储蓄卡",
+      "description": "95566 -> 1 -> 1 -> 0",
+      "logo": "../static/images/logo.jpeg",
+      "phoneNumber": "95566"
+    },
+    {
+      "id": "02",
       "name": "兴业银行信用卡",
-      "description": "123",
+      "description": "95561 -> 1 -> 8 -> 1",
       "logo": "",
       "phoneNumber": "95561"
     },
     {
-      "id": "02",
+      "id": "03",
       "name": "招商银行金葵花",
-      "description": "1245",
+      "description": "95555",
       "logo": "",
-      "phoneNumber": "95555"
+      "phoneNumber": "95555 "
     },
     {
-      "id": "03",
+      "id": "04",
+      "name": "招商银行信用卡",
+      "description": "400-820-5555",
+      "logo": "",
+      "phoneNumber": "4008205555"
+    },
+    {
+      "id": "05",
       "name": "交通银行信用卡",
-      "description": "1345",
+      "description": "4008666888 -> 1 -> 查询密码 -> 0",
       "logo": "",
       "phoneNumber": "4008666888"
     }
