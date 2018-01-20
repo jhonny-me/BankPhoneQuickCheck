f09811e103a9859d26c2dffbfcd227eb7e95fe75
Johnny Gu <johnny@wiredcraft.com>
2018-01-20 20:24:21 +0800

    added logos

--- BankPhoneQuickCheck/src/constants/data.js
+++ BankPhoneQuickCheck/src/constants/data.js
@@ -6,44 +6,58 @@ module.exports = function () {
     {
       "id": "00",
       "name": "中国银行白金信用卡",
-      "description": "40066-95569 -> 1 -> 0",
-      "logo": "../static/images/logo.jpeg",
+      "description": "4006695569 - 1 - 0",
+      "logo": "../static/images/bank_logos/logo_zhongguo.png",
       "phoneNumber": "4006695569"
     },
     {
       "id": "01",
       "name": "中国银行储蓄卡",
-      "description": "95566 -> 1 -> 1 -> 0",
-      "logo": "../static/images/logo.jpeg",
+      "description": "95566 - 1 - 1 - 0",
+      "logo": "../static/images/bank_logos/logo_zhongguo.png",
       "phoneNumber": "95566"
     },
     {
       "id": "02",
       "name": "兴业银行信用卡",
-      "description": "95561 -> 1 -> 8 -> 1",
-      "logo": "",
+      "description": "95561 - 1 - 8 - 1",
+      "logo": "../static/images/bank_logos/logo_xingye.jpg",
       "phoneNumber": "95561"
     },
     {
       "id": "03",
       "name": "招商银行金葵花",
       "description": "95555",
-      "logo": "",
+      "logo": "../static/images/bank_logos/logo_zhaoshang.gif",
       "phoneNumber": "95555 "
     },
     {
       "id": "04",
       "name": "招商银行信用卡",
-      "description": "400-820-5555",
-      "logo": "",
+      "description": "4008205555",
+      "logo": "../static/images/bank_logos/logo_zhaoshang.gif",
       "phoneNumber": "4008205555"
     },
     {
       "id": "05",
       "name": "交通银行信用卡",
-      "description": "4008666888 -> 1 -> 查询密码 -> 0",
-      "logo": "",
+      "description": "4008666888 - 1 - 查询密码 - 0",
+      "logo": "../static/images/bank_logos/logo_jiaotong.png",
       "phoneNumber": "4008666888"
+    },
+    {
+      "id": "06",
+      "name": "民生银行储蓄卡",
+      "description": "95568 - 1 - 1 - 卡号 - 0",
+      "logo": "../static/images/bank_logos/logo_minsheng.gif",
+      "phoneNumber": "95568"
+    },
+    {
+      "id": "07",
+      "name": "民生银行信用卡",
+      "description": "4006695568-1-1-0",
+      "logo": "../static/images/bank_logos/logo_minsheng.gif",
+      "phoneNumber": "4006695568"
     }
   ]
 }
