6db84f392d9876e5a6b7b1c45882b0fa2af5a85e
Johnny Gu <johnny@wiredcraft.com>
2018-01-20 19:02:52 +0800

    add author info block

---
+++ BankPhoneQuickCheck/src/constants/authorInfo.js
@@ -0,0 +1,10 @@
+/**
+ * Created by johnny on 19/01/2018.
+ */
+
+module.exports = {
+  name: '顾强 Johnny',
+  description: '一个持续学习的程序员，常旅客实践者，街舞爱好者，房市新人，币圈小白，欢迎大家一起交流。我的公众号: `奔三的红粉小`',
+  ctatitle: '联系我',
+  logo: '../static/images/authorLogo.jpeg'
+}
