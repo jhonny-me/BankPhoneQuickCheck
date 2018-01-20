905cbe9b441f88c59d9ff370c3f9ebbacf730082
Johnny Gu <johnny@wiredcraft.com>
2018-01-21 03:04:58 +0800

    update data

--- BankPhoneQuickCheck/src/constants/data.js
+++ BankPhoneQuickCheck/src/constants/data.js
@@ -26,38 +26,136 @@ module.exports = function () {
     },
     {
       "id": "03",
-      "name": "招商银行储蓄卡",
-      "description": "95555",
-      "logo": "../static/images/bank_logos/logo_zhaoshang.gif",
-      "phoneNumber": "95555 - 3 - 5 - 9 - 卡号"
+      "name": "兴业银行储蓄卡",
+      "description": "95561 - 2 - 8",
+      "logo": "../static/images/bank_logos/logo_xingye.jpg",
+      "phoneNumber": "95561"
     },
     {
       "id": "04",
-      "name": "招商银行信用卡",
-      "description": "4008205555",
+      "name": "招商银行储蓄卡",
+      "description": "95555 - 3 - 5 - 9 - 卡号",
       "logo": "../static/images/bank_logos/logo_zhaoshang.gif",
-      "phoneNumber": "4008205555 - 密码 - 9 - 1 - 证件号码后四位"
+      "phoneNumber": "95555"
     },
     {
       "id": "05",
+      "name": "招商银行信用卡",
+      "description": "4008205555 - 密码 - 9 - 1 - 证件号码后四位",
+      "logo": "../static/images/bank_logos/logo_zhaoshang.gif",
+      "phoneNumber": "4008205555"
+    },
+    {
+      "id": "06",
+      "name": "交通银行储蓄卡",
+      "description": "95559 - 0 - 5 - #",
+      "logo": "../static/images/bank_logos/logo_jiaotong.png",
+      "phoneNumber": "95559"
+    },
+    {
+      "id": "07",
       "name": "交通银行信用卡",
       "description": "4008666888 - 1 - 查询密码 - 0",
       "logo": "../static/images/bank_logos/logo_jiaotong.png",
       "phoneNumber": "4008666888"
     },
     {
-      "id": "06",
+      "id": "08",
       "name": "民生银行储蓄卡",
       "description": "95568 - 1 - 1 - 卡号 - 0",
       "logo": "../static/images/bank_logos/logo_minsheng.gif",
       "phoneNumber": "95568"
     },
     {
-      "id": "07",
+      "id": "09",
       "name": "民生银行信用卡",
       "description": "4006695568-1-1-0",
       "logo": "../static/images/bank_logos/logo_minsheng.gif",
       "phoneNumber": "4006695568"
+    },
+    {
+      "id": "10",
+      "name": "浦发银行储蓄卡",
+      "description": "95528 - 1 - 0 - 2 - 9",
+      "logo": "../static/images/bank_logos/logo_pufa.png",
+      "phoneNumber": "95528"
+    },
+    {
+      "id": "11",
+      "name": "浦发银行信用卡",
+      "description": "4008208788 - # - 0",
+      "logo": "../static/images/bank_logos/logo_pufa.png",
+      "phoneNumber": "4008208788"
+    },
+    {
+      "id": "12",
+      "name": "农业银行储蓄卡",
+      "description": "95599 - 0 - 2",
+      "logo": "../static/images/bank_logos/logo_nongye.png",
+      "phoneNumber": "95599"
+    },
+    {
+      "id": "13",
+      "name": "农业银行储蓄卡",
+      "description": "4006695599 - 卡号 - 0",
+      "logo": "../static/images/bank_logos/logo_nongye.png",
+      "phoneNumber": "4006695599"
+    },
+    {
+      "id": "14",
+      "name": "民生银行储蓄卡",
+      "description": "95568 - 1 - 1 - 卡号 - 0",
+      "logo": "../static/images/bank_logos/logo_minsheng.gif",
+      "phoneNumber": "95568"
+    },
+    {
+      "id": "15",
+      "name": "民生银行信用卡",
+      "description": "4006695568 - 1 - 1- 0",
+      "logo": "../static/images/bank_logos/logo_minsheng.gif",
+      "phoneNumber": "4006695568"
+    },
+    {
+      "id": "16",
+      "name": "工商银行储蓄卡",
+      "description": "95588 - 等会 - 9 - 2",
+      "logo": "../static/images/bank_logos/logo_gongshang.png",
+      "phoneNumber": "95588"
+    },
+    {
+      "id": "17",
+      "name": "工商银行信用卡",
+      "description": "95588 - 6 - 卡号 - 9",
+      "logo": "../static/images/bank_logos/logo_gongshang.png",
+      "phoneNumber": "95588"
+    },
+    {
+      "id": "18",
+      "name": "建设银行储蓄卡",
+      "description": "95533 - 1 - 0",
+      "logo": "../static/images/bank_logos/logo_jianshe.png",
+      "phoneNumber": "95533"
+    },
+    {
+      "id": "19",
+      "name": "建设银行信用卡",
+      "description": "4008200588 - 卡号 - 0",
+      "logo": "../static/images/bank_logos/logo_jianshe.png",
+      "phoneNumber": "4008200588"
+    },
+    {
+      "id": "20",
+      "name": "广发银行储蓄卡",
+      "description": "95508",
+      "logo": "../static/images/bank_logos/logo_guangfa.png",
+      "phoneNumber": "95508"
+    },
+    {
+      "id": "21",
+      "name": "广发银行信用卡",
+      "description": "95508",
+      "logo": "../static/images/bank_logos/logo_guangfa.png",
+      "phoneNumber": "95508"
     }
   ]
 }
