e0d6a2007a3de676b89d33081bfbcdb095a3448a
Johnny Gu <johnny@wiredcraft.com>
2018-01-21 10:08:38 +0800

    update info

--- BankPhoneQuickCheck/src/constants/authorInfo.js
+++ BankPhoneQuickCheck/src/constants/authorInfo.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   name: '顾强 Johnny',
-  description: '一个持续学习的程序员，常旅客实践者，街舞爱好者，房市新人，币圈小白，欢迎大家一起交流。我的公众号: `奔三的红粉小`',
+  description: '一个持续学习的程序员，常旅客实践者，街舞爱好者，房市新人，链圈小白，欢迎大家一起交流。我的公众号: `奔三的红粉小`',
   ctatitle: '联系我',
   logo: '../static/images/authorLogo.jpeg'
 }
